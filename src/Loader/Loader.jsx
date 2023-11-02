61b5c03ab4e9f515674c568719d43ca64d8472db
kashifhussain007 <148847037+kashifhussain007@users.noreply.github.com>
2023-11-02 10:39:53 +0500

    loaderChanges

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,3 +1,30 @@
+// import React from 'react';
+// import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+
+// const Loader = () => {
+//   const spans = [];
+
+//   for (let i = 1; i <= 20; i++) {
+//     spans.push(<span style={{'--i': i}} key={i}></span>);
+//   }
+
+//   return (
+//     <div className='container '>
+//       <div className='loadingBgSty'>
+//         <div className="loader">
+//           {spans}
+//           <div className="plane">
+//             <AirplanemodeActiveIcon/>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+
 import React from 'react';
 import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
 
@@ -5,16 +32,35 @@ const Loader = () => {
   const spans = [];
 
   for (let i = 1; i <= 20; i++) {
-    spans.push(<span style={{'--i': i}} key={i}></span>);
+    spans.push(<span style={{ '--i': i }} key={i}></span>);
   }
 
+  const planeIconStyle = {
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    width: '7em',
+    height: '2em',
+    color: 'var(--color-blue)',
+    display: 'inline-block',
+    fill: 'currentColor',
+    WebkitFlexShrink: 0,
+    MsFlexNegative: 0,
+    flexShrink: 0,
+    WebkitTransition: 'fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    transition: 'fill 200ms cubic-bezier(0.5, 0, 0.2, 1) 0ms',
+    fontSize: '1.5rem',
+    transform: 'rotate(150deg)',
+  };
+
   return (
-    <div className='container '>
-      <div className='loadingBgSty'>
+    <div className="container">
+      <div className="loadingBgSty">
         <div className="loader">
           {spans}
           <div className="plane">
-            <AirplanemodeActiveIcon/>
+            <AirplanemodeActiveIcon style={planeIconStyle} />
           </div>
         </div>
       </div>
